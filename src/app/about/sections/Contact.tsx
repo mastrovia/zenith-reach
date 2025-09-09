278ffea060e77e587482219638cedb76922ec184
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-09-09 14:57:39 +0530

    refactor: update layout and styling for improved responsiveness and consistency across sections

--- src/app/about/sections/Contact.tsx
+++ src/app/about/sections/Contact.tsx
@@ -1,12 +1,12 @@
 export default function Contact() {
   return (
-    <section className="max-w-section mx-auto pb-20 grid grid-cols-2 gap-10">
+    <section className="max-w-section mx-auto pb-20 grid sm:grid-cols-2 gap-10">
       <picture>
         <img src="/image/about-us-coconut-tree.png" alt="" />
       </picture>
-      <div className="flex flex-col justify-end">
+      <div className="flex flex-col text-center sm:text-start sm:justify-end">
         <div>
-          <h2 className="text-4xl md:text-6xl font-medium text-white mb-6 md:leading-20">
+          <h2 className="text-4xl md:text-6xl font-medium text-white mb-6 leading-13 sm:leading-normal md:leading-20">
             Want to <br /> partner with <br /> Zenith Reach?
           </h2>
           <button className="bg-primary px-5 py-2">Contact now</button>
